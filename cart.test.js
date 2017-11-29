4c3900bb5da69bd79c31fc03cee31a1f30fade3f
AJ Larson <aj.larson@devmounta.in>
2017-11-29 09:30:56 -0700

    adding solution

--- cart.test.js
+++ cart.test.js
@@ -1,74 +1,127 @@
 const cart = require('./cart');
 const cars = require('./data/cars');
 
-describe('Cart Properties:', function() {
-  test('Cart should default to an empty array.', function() {
-    expect( Array.isArray( cart.cart ) ).toEqual( true );
-    expect( cart.cart ).toEqual( [] );
-    expect( cart.cart.length ).toEqual( 0 );
-  });
-  
-  test('Total should default to 0.', function() {
-    expect( cart.total ).toEqual( 0 );
-  });
+describe('Cart Properties:', function () {
+    test('Cart should default to an empty array.', function () {
+        expect(Array.isArray(cart.cart)).toEqual(true);
+        expect(cart.cart).toEqual([]);
+        expect(cart.cart.length).toEqual(0);
+    });
+
+    test('Total should default to 0.', function () {
+        expect(cart.total).toEqual(0);
+    });
+    
+    test('oldCart should default to an empty array.', function () {
+        expect(Array.isArray(cart.oldCart)).toEqual(true);
+        expect(cart.oldCart).toEqual([]);
+        expect(cart.oldCart.length).toEqual(0);
+    });
+
+    test('oldTotal should default to 0.', function () {
+        expect(cart.oldTotal).toEqual(0);
+    });
 });
 
 
-describe('Cart Methods:', function() {
-  afterEach(function() {
-    cart.cart = [];
-    cart.total = 0;
-  });
+describe('Cart Methods:', function () {
+    afterEach(function () {
+        cart.cart = [];
+        cart.total = 0;
+        cart.oldCart = [];
+        cart.total = 0;
+    });
 
-  test('addToCart() should add a car object to the cart array.', function() {
-    cart.addToCart( cars[0] );
-    cart.addToCart( cars[1] );
+    test('addToCart() should add a car object to the cart array.', function () {
+        cart.addToCart(cars[0]);
+        cart.addToCart(cars[1]);
 
-    expect( cart.cart.length ).toEqual( 2 );
-    expect( cart.cart[0] ).toEqual( cars[0] );
-    expect( cart.cart[1] ).toEqual( cars[1] );
-  });
+        expect(cart.cart.length).toEqual(2);
+        expect(cart.cart[0]).toEqual(cars[0]);
+        expect(cart.cart[1]).toEqual(cars[1]);
+    });
 
-  test('addToCart() should increase the total property.', function() {
-    cart.addToCart( cars[0] );
-    cart.addToCart( cars[8] );
-    cart.addToCart( cars[2] );
+    test('addToCart() should increase the total property.', function () {
+        cart.addToCart(cars[0]);
+        cart.addToCart(cars[8]);
+        cart.addToCart(cars[2]);
 
-    expect( cart.total ).toEqual( cars[0].price + cars[8].price + cars[2].price );
-  });
-  
-  test('removeFromCart() should remove a car object from the cart array.', function() {
-    cart.addToCart( cars[0] );
-    cart.addToCart( cars[1] );
-    cart.addToCart( cars[2] );
-  
-    cart.removeFromCart( 1, cars[1].price );
-  
-    expect( cart.cart.length ).toEqual( 2 );
-    expect( cart.cart[0] ).toEqual( cars[0] );
-    expect( cart.cart[1] ).toEqual( cars[2] );
-  });
+        expect(cart.total).toEqual(cars[0].price + cars[8].price + cars[2].price);
+    });
 
-  test('removeFromCart() should decrease the total property.', function() {
-    cart.addToCart( cars[0] );
-    cart.addToCart( cars[8] );
-    cart.addToCart( cars[2] );
+    test('removeFromCart() should remove a car object from the cart array.', function () {
+        cart.addToCart(cars[0]);
+        cart.addToCart(cars[1]);
+        cart.addToCart(cars[2]);
 
-    cart.removeFromCart( 0, cars[0].price );
-    cart.removeFromCart( 1, cars[2].price );
+        cart.removeFromCart(1, cars[1].price);
 
-    expect( cart.total ).toEqual( cars[8].price );
-  });
+        expect(cart.cart.length).toEqual(2);
+        expect(cart.cart[0]).toEqual(cars[0]);
+        expect(cart.cart[1]).toEqual(cars[2]);
+    });
 
-  test('checkout() shoud empty the cart array and set total to 0.', function() {
-    cart.addToCart( cars[0] );
-    cart.addToCart( cars[1] );
-    cart.addToCart( cars[2] );
-    cart.addToCart( cars[3] );
+    test('removeFromCart() should decrease the total property.', function () {
+        cart.addToCart(cars[0]);
+        cart.addToCart(cars[8]);
+        cart.addToCart(cars[2]);
 
-    cart.checkout();
+        cart.removeFromCart(0, cars[0].price);
+        cart.removeFromCart(1, cars[2].price);
 
-    expect( cart.cart.length ).toEqual( 0 );
-    expect( cart.total ).toEqual( 0 );
-  });
+        expect(cart.total).toEqual(cars[8].price);
+    });
+
+    test('checkout() should empty the cart array and set total to 0.', function () {
+        cart.addToCart(cars[0]);
+        cart.addToCart(cars[1]);
+        cart.addToCart(cars[2]);
+        cart.addToCart(cars[3]);
+
+        cart.checkout();
+
+        expect(cart.cart.length).toEqual(0);
+        expect(cart.total).toEqual(0);
+    });
+
+    test('checkout() should copy the cart array and total to the oldCart and oldTotal.', function () {
+        cart.addToCart(cars[0]);
+        cart.addToCart(cars[1]);
+        cart.addToCart(cars[2]);
+        cart.addToCart(cars[3]);
+
+        cart.checkout();
+
+        expect(cart.oldCart.length).toEqual(4);
+        expect(cart.oldCart[0]).toEqual(cars[0]);
+        expect(cart.oldTotal).toEqual(cars[0].price + cars[1].price + cars[2].price + cars[3].price);
+    });
+
+    test('failedCharge() will set cart and total equal to oldCart and oldTotal.', function () {
+        cart.oldCart.push(cars[0]);
+        cart.oldCart.push(cars[1]);
+        cart.oldCart.push(cars[2]);
+        cart.oldCart.push(cars[3]);
+        cart.oldTotal = cars[0].price + cars[1].price + cars[2].price + cars[3].price;
+
+        cart.failedCharge();
+
+        expect(cart.cart.length).toEqual(4);
+        expect(cart.cart[0]).toEqual(cars[0]);
+        expect(cart.total).toEqual(cars[0].price + cars[1].price + cars[2].price + cars[3].price);
+    });
+
+    test('failedCharge() will empty the oldCart and oldTotal.', () => {
+        cart.oldCart.push(cars[0]);
+        cart.oldCart.push(cars[1]);
+        cart.oldCart.push(cars[2]);
+        cart.oldCart.push(cars[3]);
+        cart.oldTotal = cars[0].price + cars[1].price + cars[2].price + cars[3].price;
+
+        cart.failedCharge();
+
+        expect(cart.oldCart.length).toEqual(0);
+        expect(cart.oldTotal).toEqual(0);
+    });
+
 });
